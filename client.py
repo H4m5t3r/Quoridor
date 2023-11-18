4d52ff536a3b29d3807a16bb4fd962e94f75da18
Taleiko Anton V <taleiko@cubbli22-gold.local>
2023-11-18 20:24:19 +0200

    Clients now listen for incoming messages from the server

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+import sys
 
 HEADER = 64
 PORT = 5050
@@ -6,11 +8,10 @@ SERVER = '192.168.100.16'
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# input()
-server.connect(ADDR)
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.connect(ADDR)
 
-def send(msg):
+def send(server, msg):
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
@@ -19,13 +20,37 @@ def send(msg):
     server.send(message)
     print(server.recv(2048))
 
-send('Hello world!')
-input()
-send('Hello again!')
-input()
-send('Gotta go eat')
-input()
-send("!DISCONNECT")
+def fetchIncoming():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect(ADDR)
+    global active
+    while active:
+        print(server.recv(2048))
+    send(server, "!DISCONNECT")
+    
+
+def performTasks():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect(ADDR)
+    send(server, 'Hello world!')
+    input()
+    send(server, 'Hello again!')
+    input()
+    send(server, 'Gotta go eat')
+    input()
+    send(server, "!DISCONNECT")
+    global active
+    active = False
+
+
+global active
+active = True
+
+msg_check_thread = threading.Thread(target=fetchIncoming)
+msg_check_thread.start()
+
+msg_check_thread = threading.Thread(target=performTasks)
+msg_check_thread.start()
 
 # if __name__ == '__main__':
     # input()
